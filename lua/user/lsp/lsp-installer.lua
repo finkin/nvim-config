15dca822ad314ded147c0a79b6ed774cd9ae8b6d
finkin <arlekias@gmail.com>
2022-11-16 11:48:59 +0100

    [lsp] Add yamlls

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -1,37 +1,40 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-	return
+local ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not ok then
+  return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
+lsp_installer.setup {}
+local lspconfig = require("lspconfig")
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
 
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+lspconfig.sumneko_lua.setup(opts)
 
-	 if server.name == "pyright" then
-	 	local pyright_opts = require("user.lsp.settings.pyright")
-	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 end
+local jsonls_opts = require("user.lsp.settings.jsonls")
+opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+lspconfig.jsonls.setup(opts)
 
-     if server.name == "terraformls" then
-        local terraformls_opts = require("user.lsp.settings.terraformls")
-        opts = vim.tbl_deep_extend("force", terraformls_opts, opts)
-    end
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+local pyright_opts = require("user.lsp.settings.pyright")
+opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+lspconfig.pyright.setup(opts)
 
+local terraformls_opts = require("user.lsp.settings.terraformls")
+opts = vim.tbl_deep_extend("force", terraformls_opts, opts)
+lspconfig.terraformls.setup(opts)
+
+local yamlls_opts = require("user.lsp.settings.yamlls")
+opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+lspconfig.yamlls.setup(opts)
+
+local function on_attach()
+  -- setup buffer keymaps
+end
+
+for _, server in ipairs { "gopls", "html", "marksman", "omnisharp", "tflint", "yamlls"  } do
+  lspconfig[server].setup { on_attach = on_attach }
+end
