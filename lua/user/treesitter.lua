69dc2c140d534a915b394c269f921e061f92534c
finkin <arlekias@gmail.com>
2023-03-13 22:52:51 +0100

    style(treesitter): formatting

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,29 +1,20 @@
 local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go" },
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
+configs.setup({
   auto_install = true,
-  autopairs = {
-      enable = true,
-  },
+  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go", "terraform" },
+  sync_install = false,
+  ignore_install = { "" },
+  autopairs = { enable = true },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
+    disable = { "" },
     additional_vim_regex_highlighting = true,
-
   },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-  },
+  indent = { enable = true },
+  context_commentstring = { enable = true },
   rainbow = {
     enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true,  -- Also highlight non-bracket delimiters like html tags
-    max_file_lines = nil,  -- Do not enable for files with more than n lines, int
-    -- colors = {},  -- table of hex strings
-    -- termcolors = {} -- table of color name strings
-  }
-}
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+  },
+})
