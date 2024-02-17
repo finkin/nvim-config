e990f55535e567d4fd9ad464d7fdb12da752b5cf
finkin <arlekias@gmail.com>
2024-02-17 21:04:46 +0100

    [nvim-tree] Leave only options that have different value than default

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -22,61 +22,13 @@ end
 nvim_tree.setup {
   on_attach = on_attach,
   disable_netrw = true,
-  hijack_netrw = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  renderer = {
-        icons = {
-          glyphs = {
-            default = "",
-            symlink = "",
-            git = {
-              unstaged = "",
-              staged = "S",
-              unmerged = "",
-              renamed = "➜",
-              deleted = "",
-              untracked = "U",
-              ignored = "◌",
-            },
-            folder = {
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-              symlink = "",
-            },
-          },
-      },
-  },
+  sync_root_with_cwd = true,
   diagnostics = {
     enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
   },
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_root = true,
     ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    side = "left",
-    number = false,
-    relativenumber = false,
   },
 }
