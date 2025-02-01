ad1d93606423b062bd76b3de5480a1f80f71efbd
finkin <arlekias@gmail.com>
2025-02-01 13:08:02 +0100

    Fix spaces vs tabs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -17,83 +17,83 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	"nvim-lua/popup.nvim", -- Implementation of the Popup API from vim
-	"nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
-	"windwp/nvim-autopairs", -- Autopair, integrates with cmp ans treesitter
-	"numToStr/Comment.nvim", -- Easyliy comment stuff
+  "nvim-lua/popup.nvim", -- Implementation of the Popup API from vim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
+  "windwp/nvim-autopairs", -- Autopair, integrates with cmp ans treesitter
+  "numToStr/Comment.nvim", -- Easyliy comment stuff
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 
-	"akinsho/bufferline.nvim", -- Buffers as tabs
-	"moll/vim-bbye", -- Close buffers elegantly
-	"nvim-lualine/lualine.nvim",
-	"tpope/vim-surround",
+  "akinsho/bufferline.nvim", -- Buffers as tabs
+  "moll/vim-bbye", -- Close buffers elegantly
+  "nvim-lualine/lualine.nvim",
+  "tpope/vim-surround",
 
-	-- Themes
-	{ "dracula/vim", as = "dracula" },
-	{ "catppuccin/nvim", as = "catppuccin" },
-	"folke/tokyonight.nvim",
+  -- Themes
+  { "dracula/vim", as = "dracula" },
+  { "catppuccin/nvim", as = "catppuccin" },
+  "folke/tokyonight.nvim",
 
-	-- Completion
-	"hrsh7th/nvim-cmp", -- Completion plugin
-	"hrsh7th/cmp-buffer", -- buffer completion
-	"hrsh7th/cmp-path", -- path completion
-	"hrsh7th/cmp-cmdline", -- cmdline completion
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-nvim-lua",
-	"hrsh7th/cmp-nvim-lsp-signature-help",
+  -- Completion
+  "hrsh7th/nvim-cmp", -- Completion plugin
+  "hrsh7th/cmp-buffer", -- buffer completion
+  "hrsh7th/cmp-path", -- path completion
+  "hrsh7th/cmp-cmdline", -- cmdline completion
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lua",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
 
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
 
-	"L3MON4D3/LuaSnip", -- snippet engine
-	"saadparwaiz1/cmp_luasnip",
-	"rafamadriz/friendly-snippets", -- a bunch of snippets
+  "L3MON4D3/LuaSnip", -- snippet engine
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets", -- a bunch of snippets
 
-	-- LSP
-	"neovim/nvim-lspconfig", -- enable LSP
-	"jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
-	"arkav/lualine-lsp-progress",
-	"glepnir/lspsaga.nvim",
-	"folke/trouble.nvim",
+  -- LSP
+  "neovim/nvim-lspconfig", -- enable LSP
+  "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
+  "arkav/lualine-lsp-progress",
+  "glepnir/lspsaga.nvim",
+  "folke/trouble.nvim",
 
-	-- DAP
-	"mfussenegger/nvim-dap",
+  -- DAP
+  "mfussenegger/nvim-dap",
 
-	-- Command line
-	{
-		"folke/noice.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-	},
+  -- Command line
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
 
-	-- Telescope
-	"nvim-telescope/telescope.nvim",
-	"nvim-telescope/telescope-media-files.nvim",
-	"nvim-telescope/telescope-project.nvim",
-	"nvim-telescope/telescope-ui-select.nvim",
+  -- Telescope
+  "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope-media-files.nvim",
+  "nvim-telescope/telescope-project.nvim",
+  "nvim-telescope/telescope-ui-select.nvim",
 
-	-- Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
-	"JoosepAlviste/nvim-ts-context-commentstring",
-	"fladson/vim-kitty",
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  "fladson/vim-kitty",
 
-	-- Git
-	"lewis6991/gitsigns.nvim",
+  -- Git
+  "lewis6991/gitsigns.nvim",
 
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
   }
-	-- use "dstein64/vim-startuptime"
+  -- use "dstein64/vim-startuptime"
 })
