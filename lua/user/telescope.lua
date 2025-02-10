557f97f3016c57c9ac5464d54f39f51bfaf62df0
finkin <arlekias@gmail.com>
2025-02-10 14:19:34 +0100

    feat(telescope) show hidden files while finding in files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -102,13 +102,11 @@ telescope.setup {
     },
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    live_grep = {
+      additional_args = function ()
+        return {"--hidden"}
+      end
+    }
   },
   extensions = {
     media_files = {
