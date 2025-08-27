f46b61c58ab60bd21bbc1eb12f0be4b87fdf36ea
finkin <arlekias@gmail.com>
2025-08-28 00:45:55 +0200

    [treesitter] fix config after new changes

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,14 +1,25 @@
 local configs = require("nvim-treesitter.configs")
+
 configs.setup({
   auto_install = true,
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go", "terraform" },
+  ensure_installed = {
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "bash",
+    "python",
+    "go",
+    "terraform"
+  },
   sync_install = false,
   ignore_install = { "" },
-  autopairs = { enable = true },
+  modules = {},
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" },
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true },
+  indent = {
+    enable = true
+  },
 })
