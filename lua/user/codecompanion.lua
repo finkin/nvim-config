cca8f81a39082e7def71d5b46372fcd0ea44652e
finkin <arlekias@gmail.com>
2025-08-27 14:07:36 +0200

    Changes to CodeCompanion

--- lua/user/codecompanion.lua
+++ lua/user/codecompanion.lua
@@ -1,4 +1,14 @@
 require("codecompanion").setup({
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        make_vars = true,
+        make_slash_commands = true,
+        show_result_in_chat = true
+      }
+    }
+  },
   strategies = {
     chat = {
       adapter = "anthropic",
